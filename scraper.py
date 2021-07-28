b1dcae7fc99679145291204e2fc971612a24f3e0
Cédric PAPIN <cedricpapin1@orange.fr>
2021-07-28 22:15:28 +0200

    Code MAJ

--- scraper.py
+++ scraper.py
@@ -5,15 +5,31 @@ url = 'http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_
 
 response = requests.get(url)
 
-def get_title(soup):
-	title = soup.find('title')
-	return title.text
+def get_product_page_url():
+	return response.url
 
 def get_upc(soup):
-	upc = soup.find('title')
+	upc = soup.find('td')
+	return upc.text
+
+def get_title(soup):
+	title = soup.find('h1')
 	return title.text
+
+def get_price_including_tax(soup):
+	price_including_tax = soup.find('td')
+	return price_including_tax.text
+
+
 
 if response.ok:
 	soup = BeautifulSoup(response.text, 'html.parser')
+	product_page_url = get_product_page_url()
+	upc = get_upc(soup)
 	title = get_title(soup)
+	price_including_tax = get_price_including_tax(soup)
+	print(product_page_url)
+	print(upc)
 	print(title)
+	print(price_including_tax)
+	
