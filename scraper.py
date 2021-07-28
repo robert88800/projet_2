95b739fb211235a9cd01b85b5d663daa856d8509
robert88800 <cedricpapin1@orange.fr>
2021-07-28 20:30:50 +0200

    Ajout des modifs

--- scraper.py
+++ scraper.py
@@ -1,11 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/catalogue/category/books/history_32/index.html'
+url = 'http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html'
 
 response = requests.get(url)
 
+def get_title(soup):
+	title = soup.find('title')
+	return title.text
+
+def get_upc(soup):
+	upc = soup.find('title')
+	return title.text
+
 if response.ok:
 	soup = BeautifulSoup(response.text, 'html.parser')
-	title = soup.find('title')
-	print(title.text)
+	title = get_title(soup)
+	print(title)
