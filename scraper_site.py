8ccc2e3ab949d6ea47324b80a291fa6d615cdc6e
Cédric PAPIN <cedricpapin1@orange.fr>
2021-08-18 10:43:41 +0200

    refactor: remove scraper_category & scraper_site

--- scraper_site.py
+++
@@ -1,8 +0,0 @@
-from lib_site import extract_site_categories_url, transform_site_data
-
-url = 'http://books.toscrape.com'
-
-list_site_categories_url = extract_site_categories_url(url)
-
-transform_site_data(list_site_categories_url)
-
